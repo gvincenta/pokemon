cf865628ea874bcf1d33ea1547c60af628dd7121
gvincenta <gilbert11323@gmail.com>
2021-06-21 15:14:42 +1000

    refactor components into table

--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -1,33 +1,112 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-export default function List({ data, columns }) {
-    console.log({ data });
+const PAGE_LIMIT_OPTIONS = [1, 5, 10, 15, 20];
+export const PAGE_LIMIT = 20;
+
+export default function List({
+    data,
+    columns,
+    actions = [],
+    total,
+    hasNextPage,
+    onChangePage,
+}) {
+    const [page, setPage] = useState(0);
+    const [limit, setLimit] = useState(PAGE_LIMIT);
+
+    const onPreviousPage = () => {
+        onChangePage && onChangePage(page - 1, limit);
+        setPage(page - 1);
+    };
+    const onNextPage = () => {
+        onChangePage && onChangePage(page + 1, limit);
+        setPage(page + 1);
+    };
+
+    const onPageLimitChange = (e) => {
+        const newLimit = Number(e.target.value);
+        onChangePage && onChangePage(page, newLimit);
+        setLimit(newLimit);
+    };
+
+    const getData = () => {
+        if (onChangePage) {
+            return data;
+        }
+        const lowerBound = page * limit;
+        const upperBound = (page + 1) * limit;
+
+        return data.filter(
+            (v, index) => index < upperBound && index >= lowerBound
+        );
+    };
+
+    useEffect(() => {
+        if (onChangePage) {
+            onChangePage(page, limit);
+        }
+    }, [onChangePage]);
     return (
-        <table>
-            <thead>
-                <tr>
-                    {columns.map(({ title }, index) => (
-                        <th key={`td-${index}-${title}`}> {title} </th>
-                    ))}
-                </tr>
-            </thead>
-            <tbody>
-                {data?.length > 0
-                    ? data.map((value, index) => (
-                          <tr key={index}>
-                              {columns.map(({ accessor }) => {
-                                  //   console.log({value, accessor})
-                                  return (
-                                      <td key={`td-${index}-${accessor}`}>
-                                          {' '}
-                                          {value[accessor]}{' '}
-                                      </td>
-                                  );
-                              })}
-                          </tr>
-                      ))
-                    : 'Not Found.'}
-            </tbody>
-        </table>
+        <>
+            <p> Total: {total} </p>
+            {page > 0 && <button onClick={onPreviousPage}> Previous </button>}
+            {hasNextPage && hasNextPage(page, limit) && (
+                <button onClick={onNextPage}> Next </button>
+            )}
+            <select
+                name="pokemons per page"
+                value={limit}
+                onChange={onPageLimitChange}
+            >
+                {' '}
+                {PAGE_LIMIT_OPTIONS.map((v) => (
+                    <option key={v} value={v}>
+                        {' '}
+                        {v}{' '}
+                    </option>
+                ))}{' '}
+            </select>
+            <table>
+                <thead>
+                    <tr>
+                        {columns.map(({ title }, index) => (
+                            <th key={`td-${index}-${title}`}> {title} </th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    {data?.length > 0
+                        ? getData().map((value, index) => (
+                              <tr key={index}>
+                                  {columns.map(({ accessor, onDisplay }) => {
+                                      //   console.log({value, accessor})
+                                      return (
+                                          <td key={`td-${index}-${accessor}`}>
+                                              {' '}
+                                              {onDisplay
+                                                  ? onDisplay(value)
+                                                  : value[accessor]}{' '}
+                                          </td>
+                                      );
+                                  })}
+                                  <td>
+                                      {actions.map(({ title, onClick }) => (
+                                          <button
+                                              key={`${title}-${index}`}
+                                              onClick={(e) => {
+                                                  onClick(e, value);
+                                              }}
+                                          >
+                                              {' '}
+                                              {title}{' '}
+                                          </button>
+                                      ))}
+                                  </td>
+                              </tr>
+                          ))
+                        : 'Not Found.'}
+                </tbody>
+            </table>
+        </>
     );
 }
--- src/Pages/List.jsx
+++ src/Pages/List.jsx
@@ -1,70 +1,54 @@
 import React, { useEffect, useState } from 'react';
 import { getAllPokemons } from '../api';
+import { getOwnedTotals } from '../pokemon';
+import Table from '../Components/Table';
 
-const PAGE_LIMIT = 20;
-
-const hasNextPage = (count, limit, currentPage) => {
-    console.log({ count, limit, currentPage });
-    return currentPage < count / limit;
-};
 export default function List() {
     const [data, setData] = useState({
         count: 0,
         next: '',
         previous: null,
         results: [],
-        limit: PAGE_LIMIT,
     });
 
-    const [page, setPage] = useState(0);
-
-    const previousPage = () => setPage(page - 1);
-    const nextPage = () => setPage(page + 1);
-
-    useEffect(() => {
-        //fetch a list of pokemons
-
-        getAllPokemons(page * data.limit, data.limit)
+    const onChangePage = (page, limit) => {
+        getAllPokemons(page * limit, limit)
             .then((res) => {
                 const { count, next, previous, results } = res;
-                setData({ ...data, count, next, previous, results });
+                setData({
+                    ...data,
+                    count,
+                    next,
+                    previous,
+                    results: getOwnedTotals(results),
+                });
             })
             .catch((e) => {
                 console.log('ERROR', { e });
             });
-    }, [page]);
+    };
 
     return (
         <div>
-            <p> Total: {data.count} </p>
-            {page > 0 && <button onClick={previousPage}> Previous </button>}
-            {data.next && <button onClick={nextPage}> Next </button>}
-            {/* dropdown for page limit */}
-            <table>
-                <thead>
-                    <tr>
-                        <td> Name </td>
-                        <td> Owned Total </td>
-                    </tr>
-                </thead>
-                <tbody>
-                    {data.results?.length > 0
-                        ? data.results.map(({ name, url }) => (
-                              <tr>
-                                  {' '}
-                                  <td>
-                                      {' '}
-                                      <a href={`/pokemon/?fetch=${url}`}>
-                                          {' '}
-                                          {name}{' '}
-                                      </a>{' '}
-                                  </td>
-                                  <td> insret counter </td>{' '}
-                              </tr>
-                          ))
-                        : 'No Pokemon Found.'}
-                </tbody>
-            </table>
+            <Table
+                data={data.results}
+                total={data.count}
+                hasNextPage={() => data.next}
+                onChangePage={onChangePage}
+                columns={[
+                    {
+                        title: 'Name',
+                        accessor: 'name',
+                        onDisplay: ({ name, url }) => (
+                            <a href={`/pokemon/?fetch=${url}`}>{name}</a>
+                        ),
+                    },
+                    {
+                        title: 'Owned Total',
+                        accessor: 'ownedTotal',
+                    },
+                ]}
+            />
         </div>
     );
 }
