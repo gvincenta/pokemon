a187e504bbf449489d03da4f862b0d0fece724bb
gvincenta <gilbert11323@gmail.com>
2021-06-24 19:04:31 +1000

    update navigation styling

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,28 +1,32 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { NavigationButton } from './Components/Button';
 import styled from '@emotion/styled';
+const Nav = styled.nav`
+    display: grid;
+    grid-template-columns: auto auto;
+`;
+
+const Div = styled.div`
+    text-align: center;
+`;
 
 export default function Navbar() {
-    const color = 'white';
-    const Nav = styled.nav`
-        background-color: #ff8f36;
-        display: grid;
-        grid-template-columns: auto auto;
-    `;
-
-    const Div = styled.div`
-        text-align: center;
-    `;
-
     return (
         <Nav>
             <Div>
                 {' '}
-                <Link to="/pokemons"> Pokemon List </Link>{' '}
+                <Link to="/pokemons">
+                    {' '}
+                    <NavigationButton> Pokemon List </NavigationButton>
+                </Link>{' '}
             </Div>
             <Div>
                 {' '}
-                <Link to="/mypokemons"> My Pokemon List</Link>{' '}
+                <Link to="/mypokemons">
+                    {' '}
+                    <NavigationButton> My Pokemon List </NavigationButton>{' '}
+                </Link>{' '}
             </Div>
         </Nav>
     );
