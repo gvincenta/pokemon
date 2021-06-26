29a85550673984ace986e0ff44b2b5a3ee927536
gvincenta <gilbert11323@gmail.com>
2021-06-26 19:20:17 +1000

    add loading component

---
+++ src/Components/Loading.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const Loading = styled.div`
+    font-family: PokemonSolid;
+    padding: auto;
+    margin-top: 50%;
+    text-align: center;
+    text-transform: uppercase;
+    font-size: 30px;
+`;
+
+export default Loading;
