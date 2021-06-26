87dd27f460451ef726f3c8dd26869a1f75f79b4d
gvincenta <gilbert11323@gmail.com>
2021-06-26 19:25:08 +1000

    add modal confirmation, add paginationState to keep track of pagination

--- src/Pages/MyList.jsx
+++ src/Pages/MyList.jsx
@@ -1,30 +1,60 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import Table from '../Components/Table';
-import { getMyPokemons, removePokemon, isEqual } from '../pokemon';
+import { getMyPokemons, removePokemon } from '../pokemon';
 import Navbar from '../Navbar';
-import Pagination from '../Components/Pagination';
+import Pagination, { PAGE_LIMIT } from '../Components/Pagination';
+import { DangerButton } from '../Components/Button';
+import Modal from '../Components/Modal';
 
 export default function MyList() {
     const [data, setData] = useState(getMyPokemons());
+    const [paginationState, setPaginationState] = useState({
+        page: 0,
+        limit: PAGE_LIMIT,
+    });
+    const [modal, setModal] = useState({
+        open: false,
+        content: [],
+    });
+
     const onRemovePokemon = (e, pokemon) => {
         removePokemon(pokemon);
         setData(getMyPokemons());
+        setModal({
+            open: false,
+            content: [],
+        });
     };
 
-    const getData = () => {
-        // const lowerBound = page * limit; //todo after useCOntext
-        // const upperBound = (page + 1) * limit;
-
-        return data;
-        // .filter(
-        //     (v, index) => index < upperBound && index >= lowerBound
-        // );
+    const onOpenModal = (e, pokemon) => {
+        setModal({
+            open: true,
+            content: (
+                <div style={{ textAlign: 'center' }}>
+                    {' '}
+                    Would you like to delete: <br /> {pokemon.name} -{' '}
+                    {pokemon.nickname} ? <br /> This action cannot be undone.
+                    <br />{' '}
+                    <DangerButton onClick={() => onRemovePokemon(e, pokemon)}>
+                        {' '}
+                        Yes{' '}
+                    </DangerButton>{' '}
+                </div>
+            ),
+        });
     };
+
+    const displayData = useMemo(() => {
+        const lowerBound = paginationState.page * paginationState.limit;
+        const upperBound = (paginationState.page + 1) * paginationState.limit;
+
+        return data.slice(lowerBound, upperBound);
+    }, [data, paginationState.page, paginationState.limit]);
     return (
         <div>
             <Navbar />
             <Table
-                data={getData()}
+                data={displayData}
                 columns={[
                     {
                         title: 'Name',
@@ -38,13 +68,25 @@ export default function MyList() {
                 actions={[
                     {
                         title: 'x',
-                        onClick: onRemovePokemon,
+                        onClick: onOpenModal,
                     },
                 ]}
             />
             <Pagination
                 total={data.length}
                 hasNextPage={(page, limit) => (page + 1) * limit < data.length}
+                onChangePage={(page, limit) => {
+                    setPaginationState({ page, limit });
+                }}
+            />
+            <Modal
+                {...modal}
+                onClose={() => {
+                    setModal({
+                        open: false,
+                        content: [],
+                    });
+                }}
             />
         </div>
     );
