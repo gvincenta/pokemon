1a1f5958621eda2225745e5a9a17040ec75fd960
gvincenta <gilbert11323@gmail.com>
2021-06-27 13:45:47 +1000

    add jump to page action & page caching with sessionStorage

--- src/Components/Pagination.jsx
+++ src/Components/Pagination.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
-
+import React, { useState, useEffect, useMemo } from 'react';
+import Modal, { CenteredContent, Label, Input } from '../Components/Modal';
 import styled from '@emotion/styled';
-import { TextButton } from '../Components/Button';
+import Button, { TextButton } from './Button';
 export const PAGE_LIMIT = 10; //how many items shown in a page
 //main container
 const ToolbarContainer = styled.div`
@@ -28,41 +28,94 @@ const ToolbarItemRight = styled.div`
     text-align: right;
 `;
 
-export default function Pagination({ total, hasNextPage, onChangePage }) {
-    const [page, setPage] = useState(0);
-    const [limit] = useState(PAGE_LIMIT);
+const MY_POKEMONS_KEY = 'MY_POKEMONS_KEY';
+const POKEMONS_KEY = 'POKEMONS_KEY';
+
+//use sessionStorage to cache current page number based on url.
+const getCachedPageNumber = (upperBound) => {
+    const key = window.location.hash.toLowerCase().includes('/mypokemons')
+        ? MY_POKEMONS_KEY
+        : POKEMONS_KEY;
+
+    const cachedPageNumber = sessionStorage.getItem(key);
+    //return value: 0 <= value <= upperBound
+    if (!Boolean(cachedPageNumber)) {
+        return 0;
+    }
+
+    return Math.min(Number(cachedPageNumber), upperBound);
+};
+
+const setCachedPageNumber = (pageNumber, upperBound) => {
+    const key = window.location.hash.toLowerCase().includes('/mypokemons')
+        ? MY_POKEMONS_KEY
+        : POKEMONS_KEY;
+    //set value: 0 <= value <= upperBound
+    sessionStorage.setItem(
+        key,
+        Math.min(upperBound, Math.max(0, pageNumber ?? 0))
+    );
+};
+
+export default function Pagination({ total = 0, hasNextPage, onChangePage }) {
+    const upperBound = useMemo(() => {
+        return Math.ceil(total / PAGE_LIMIT);
+    }, [total, PAGE_LIMIT]);
+
+    const [page, setPage] = useState(
+        getCachedPageNumber(Math.ceil(total / PAGE_LIMIT)) ?? 0
+    );
+    const [jumpToPage, setJumpToPage] = useState(1);
+
+    //modal for jumping to a page.
+    const [openModal, setOpenModal] = useState(false);
 
     /**
      * button actions:
      * fast rewind to first page,
      * previous page,
      * next page,
-     * fast forward to last page.
+     * fast forward to last page,
+     * jump to a page (with modal)
      */
     const onFirstPage = () => {
-        onChangePage && onChangePage(0, limit);
+        onChangePage && onChangePage(0, PAGE_LIMIT);
         setPage(0);
+        setCachedPageNumber(0, upperBound);
     };
     const onPreviousPage = () => {
-        onChangePage && onChangePage(page - 1, limit);
+        onChangePage && onChangePage(page - 1, PAGE_LIMIT);
         setPage(page - 1);
+        setCachedPageNumber(page - 1, upperBound);
     };
     const onNextPage = () => {
-        onChangePage && onChangePage(page + 1, limit);
+        onChangePage && onChangePage(page + 1, PAGE_LIMIT);
         setPage(page + 1);
+        setCachedPageNumber(page + 1, upperBound);
     };
     const onLastPage = () => {
-        onChangePage && onChangePage(Math.ceil(total / limit), limit);
-        setPage(Math.ceil(total / limit));
+        onChangePage && onChangePage(upperBound, PAGE_LIMIT);
+        setPage(upperBound);
+        setCachedPageNumber(upperBound, upperBound);
+    };
+    const onJumpPage = (e, pageNumber) => {
+        onChangePage && onChangePage(pageNumber, PAGE_LIMIT);
+        setPage(pageNumber);
+        setCachedPageNumber(pageNumber, upperBound);
+    };
+    //when page number is clicked, ask to jump to what page.
+    const onOpenModal = (e, pokemon) => {
+        setJumpToPage(page + 1); //display starts from 1, not 0.
+        setOpenModal(true);
     };
 
     useEffect(() => {
         //if total goes down and current page is no longer exist, go back previous page.
-        const lowerBound = page * limit + 1;
+        const lowerBound = page * PAGE_LIMIT + 1;
         if (page !== 0 && lowerBound > total) {
             onPreviousPage();
         }
-    }, [total, page, limit]);
+    }, [total, page, PAGE_LIMIT]);
     return (
         <ToolbarContainer>
             <ToolbarItem>
@@ -78,13 +131,20 @@ export default function Pagination({ total, hasNextPage, onChangePage }) {
             </ToolbarItem>
             <ToolbarItemCenter>
                 {' '}
-                Page {page + 1} / {Math.ceil(total / limit)}{' '}
+                Page{' '}
+                <TextButton active={true} onClick={onOpenModal}>
+                    {' '}
+                    {page + 1}{' '}
+                </TextButton>
+                / {Math.max(upperBound, 1)}{' '}
             </ToolbarItemCenter>
             <ToolbarItemRight>
                 {' '}
                 <TextButton
                     active={true}
-                    disabled={hasNextPage ? !hasNextPage(page, limit) : true}
+                    disabled={
+                        hasNextPage ? !hasNextPage(page, PAGE_LIMIT) : true
+                    }
                     onClick={onNextPage}
                 >
                     {' '}
@@ -107,13 +167,56 @@ export default function Pagination({ total, hasNextPage, onChangePage }) {
             <ToolbarItemRight>
                 <TextButton
                     active={true}
-                    disabled={hasNextPage ? !hasNextPage(page, limit) : true}
+                    disabled={
+                        hasNextPage ? !hasNextPage(page, PAGE_LIMIT) : true
+                    }
                     onClick={onLastPage}
                 >
                     {' '}
                     &raquo;
                 </TextButton>{' '}
             </ToolbarItemRight>
+            <Modal
+                open={openModal}
+                content={
+                    <CenteredContent>
+                        {' '}
+                        <Label> Jump to page:</Label>
+                        <Input
+                            type="Number"
+                            max={Math.max(upperBound, 1)}
+                            min={1}
+                            value={jumpToPage}
+                            onChange={(e) => {
+                                if (e.target.value > Math.max(upperBound, 1)) {
+                                    setJumpToPage(Math.max(upperBound, 1));
+                                }
+                                setJumpToPage(e.target.value ?? 1);
+                            }}
+                        />
+                        <br />
+                        <Button
+                            style={{ marginTop: '10px' }}
+                            onClick={
+                                (e) =>
+                                    onJumpPage(
+                                        e,
+                                        Math.min(
+                                            upperBound,
+                                            Math.max(0, jumpToPage - 1)
+                                        )
+                                    ) //display is from 1, actual paging is from 0
+                            }
+                        >
+                            {' '}
+                            Submit{' '}
+                        </Button>{' '}
+                    </CenteredContent>
+                }
+                onClose={() => {
+                    setOpenModal(false);
+                }}
+            />
         </ToolbarContainer>
     );
 }
